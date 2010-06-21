2c415128138fe2e65998758ac96ae06dc58444c9
Mike Green <mike.is.green@gmail.com>
2010-06-21 16:27:31 -0400

    added some recursive key string->symbol methods to Hash class. put them in the daemon file since that's where they're used

--- bin/printasaurus.rb
+++ bin/printasaurus.rb
@@ -6,19 +6,55 @@ require File.join(File.dirname(__FILE__), '..', 'lib', 'mail_fetcher')
 require File.join(File.dirname(__FILE__), '..', 'lib', 'file_printer')
 require 'daemons'
 
-class Application
-	attr_accessor :args
-	
-	def initialize(args)
-		@args = args
+class Hash
+	def symbolize_keys
+	  self.inject({}) do |options, (key, value)|
+	    options[(key.to_sym rescue key) || key] = value
+	    options
+	  end
 	end
 	
-	def run!
-		
+	def symbolize_keys!
+		self.replace(self.symbolize_keys)
+	end
+	
+	def recursive_symbolize_keys!
+		self.symbolize_keys!
+		self.values.each {|h| h.recursive_symbolize_keys! if h.is_a?(Hash) }
+		self.values.select {|v| v.is_a?(Array) }.flatten.each {|h| h.recursive_symbolize_keys! if h.is_a?(Hash) }
+		self
+	end
+end
+
+module Printasaurus
+	class Daemon
+		attr_accessor :args
+	
+		def initialize(args)
+			@args = args
+			@config_file = @args.shift
+		end
+	
+		def run!
+			if load_config_file
+				
+			else
+				STDERR.puts "Error loading config file"
+				exit 1
+			end
+		end
+	
+		private
+			def load_config_file
+				if FileTest.exist? @config_file
+					@config = YAML.load(File.read(@config_file)).recursive_symbolize_keys!
+				end
+				false
+			end
 	end
 end
 
 if __FILE__ == $0
-	app = Application.new(ARGV)
+	app = Daemon.new(ARGV)
 	app.run!
 end
