fd7ea45ed3bfa60f6bf1faa0f08d332bb0867266
Mike Green <mike.is.green@gmail.com>
2010-06-23 12:29:11 -0400

    forgot a newline at the end of the log formatting method

--- lib/printasaurus_logger.rb
+++ lib/printasaurus_logger.rb
@@ -2,6 +2,6 @@ require "logger"
 
 class PrintasaurusLogger < Logger
 	def format_message(severity, timestamp, progname, msg)
-		"[#{timestamp.strftime('%Y-%m-%d %H:%M:%S')}] #{severity}: #{msg}"
+		"[#{timestamp.strftime('%Y-%m-%d %H:%M:%S')}] #{severity}: #{msg}\n"
 	end
 end
